64e21ba9f7cc08c6726ed74363d97e86ea5e9edd
paulabarachosevaite <112000667+paulabarachosevaite@users.noreply.github.com>
2022-10-02 14:42:57 +0100

    Implemented authorization

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+  // Check if token exists in the header
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // Get token from the header
+      token = req.headers.authorization.split(" ")[1];
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // Get user from the token (user id)
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not authorized");
+    }
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
+});
+
+module.exports = { protect };
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 5000;
+
+// Connect to DB:
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.use("/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
